5b4ca92eff0082619a7e64c00c0034cb5bcd156f
followthewhiterabbit <3tonishere@gmail.com>
2020-05-02 17:59:12 +0300

    Implement passing ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, EventEmitter, Output } from '@angular/core'
 
 import { Recipe } from './recipe.model'
+import { Ingredient } from '../shared/ingredient.model'
+import { ShoppingListService } from '../shopping-list/shopping-list.service'
 
 @Injectable()
 export class RecipeService {
@@ -8,18 +10,26 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Test recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Test recipe2',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ]
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes(): Recipe[] {
     return this.recipes.slice()
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients)
   }
 }
