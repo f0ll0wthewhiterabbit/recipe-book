f64172a9e9c2f082917290ae4b94dd1961d7a234
followthewhiterabbit <3tonishere@gmail.com>
2020-05-09 18:40:12 +0300

    Implement automatic sign in and sign out

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   user = new BehaviorSubject<User>(null)
+  private tokenExpirationTimer: any
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -60,9 +61,51 @@ export class AuthService {
       )
   }
 
+  autoLogin() {
+    const userData: {
+      email: string
+      id: string
+      _token: string
+      _tokenExpirationDate: string
+    } = JSON.parse(localStorage.getItem('userData'))
+
+    if (!userData) {
+      return
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    )
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser)
+
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+
+      this.autoLogout(expirationDuration)
+    }
+  }
+
   logout() {
     this.user.next(null)
+    localStorage.removeItem('userData')
+
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer)
+    }
+
+    this.tokenExpirationTimer = null
     this.router.navigate(['/auth'])
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout()
+    }, expirationDuration)
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
@@ -98,5 +141,7 @@ export class AuthService {
     const user = new User(email, userId, token, expirationDate)
 
     this.user.next(user)
+    this.autoLogout(expiresIn * 1000)
+    localStorage.setItem('userData', JSON.stringify(user))
   }
 }
