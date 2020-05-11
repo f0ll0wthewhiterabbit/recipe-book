603539ac661e378ff7268d7949673c15c55d4dc6
followthewhiterabbit <3tonishere@gmail.com>
2020-05-11 14:59:35 +0300

    Implement lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,12 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
 import { AppRoutingModule } from './app-routing.module'
-import { RecipesModule } from './recipes/recipes.module'
-import { ShoppingListModule } from './shopping-list/shopping-list.module'
-import { AuthModule } from './auth/auth.module'
 import { SharedModule } from './shared/shared.module'
 import { CoreModule } from './core.module'
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    RecipesModule,
-    ShoppingListModule,
-    AuthModule,
-    SharedModule,
-    CoreModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SharedModule, CoreModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
