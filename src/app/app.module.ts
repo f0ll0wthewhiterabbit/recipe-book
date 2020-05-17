ca2d195203c7816111b404c9eb09a42044800b0a
followthewhiterabbit <3tonishere@gmail.com>
2020-05-17 18:37:53 +0300

    Introduce store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { StoreModule } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
+import { environment } from '../environments/environment'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
 import { AppRoutingModule } from './app-routing.module'
@@ -20,6 +22,7 @@ import { AuthEffects } from './auth/store/auth.effects'
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     SharedModule,
     CoreModule,
   ],
